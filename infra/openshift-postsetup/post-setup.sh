38a3a8ddee4472fb469012bf70407d81cca9d9a3
Marco Bungart <marco.bungart@googlemail.com>
2021-08-13 22:33:03 +0200

    general cleanup

--- infra/openshift-postsetup/post-setup.sh
+++ infra/openshift-postsetup/post-setup.sh
@@ -2,6 +2,10 @@
 
 # make image registry publicly available
 oc patch configs.imageregistry.operator.openshift.io/cluster --patch '{"spec":{"defaultRoute":true}}' --type=merge
+
+# install pipeline operator
 oc apply -f 00-pipelines-subscription.yml
+
+# create project and image stream
 oc new-project quarkus-devmode || oc project quarkus-devmode
 oc apply -f 01-image-streams.yml
